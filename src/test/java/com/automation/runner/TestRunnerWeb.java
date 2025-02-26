02b9f9921709784a4bc71b441dbbabee64c2aa31
Ashwani Kumar Dwivedi <ashwanidwivedi7898@gmail.com>
2025-02-26 13:00:29 +0530

    added maven configuration

--- src/test/java/com/automation/runner/TestRunnerWeb.java
+++ src/test/java/com/automation/runner/TestRunnerWeb.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com.automation.steps",
-        plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber.html", "com.cucumber.listener.ExtentCucumberFormatter:"},
+        plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         tags = "@web"
 )
 public class TestRunnerWeb {
